4dd1cbe232fa267373f2b0b508294048c9af0b31
Nadira Ali <nadiraali188a@gmail.com>
2022-05-06 20:52:32 +0100

    thought routes with CRUD operations

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,20 +1,19 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
   getUsers,
   getSingleUser,
   createUser,
   updateUser,
   deleteUser,
-} = require('../../controllers/userController');
+} = require("../../controllers/userController");
 
 // /api/users
-router.route('/').get(getUsers).post(createUser);
+router.route("/").get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser).delete(deleteUser);
+router.route("/:userId").get(getSingleUser).delete(deleteUser);
 
 // /api/users/:userId/update
-router.route('/:userId/update').put(updateUser);
+router.route("/:userId/update").put(updateUser);
 
 module.exports = router;
-
