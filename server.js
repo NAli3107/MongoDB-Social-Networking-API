2244956c7fff108e170c82418306d46b41e4e036
Nadira Ali <nadiraali188a@gmail.com>
2022-05-06 20:54:42 +0100

    routes middleware

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(require("./routes"));
+
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
