15e0064324d8dfe8feddfa2858349b2eb3e22008
Nadira Ali <nadiraali188a@gmail.com>
2022-05-06 18:32:46 +0100

    added virtual called friendCount

--- models/User.js
+++ models/User.js
@@ -1,27 +1,39 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: () => Promise.resolve(false),
-      message: "Email validation failed",
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: () => Promise.resolve(false),
+        message: "Email validation failed",
+      },
+    },
+    thoughts: {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+    friends: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
   },
-  thoughts: {
-    type: Schema.Types.ObjectId,
-    ref: "Thought",
-  },
-  friends: {
-    type: Schema.Types.ObjectId,
-        ref: 'User',
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
   }
+);
+
+userSchema.virtual('friendsCount').get(function () {
+  return this.friends.length;
 });
