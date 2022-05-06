517f1c4f1b677c278c2f9972edd5c5dbec9845e6
Nadira Ali <nadiraali188a@gmail.com>
2022-05-06 20:34:07 +0100

    create thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,16 +3,37 @@ const { User, Thought } = require("../models");
 module.exports = {
   getThoughts(req, res) {
     Thought.find()
-      .then((thoughts) => res.json(thoughts))
+      .then((thought) => res.json(thought))
       .catch((err) => res.status(500).json(err));
   },
   getSingleThought(req, res) {
     Thought.findOne({ _id: req.params.thoughtId })
-      .then((thoughts) =>
+      .then((thought) =>
         !thoughts
           ? res.status(404).json({ message: "No thought found with this ID! 😣 Try again." })
-          : res.json(thoughts)
+          : res.json(thought)
       )
       .catch((err) => res.status(500).json(err));
+  },
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $addToSet: { thought: thought._id } },
+          { new: true }
+        );
+      })
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: 'Thought created 😁, but found no user with this ID 😩. Try again!',
+            })
+          : res.json('Created the thought 🥳')
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   },
 };
