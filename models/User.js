3bdd1c5761397759d9dd06f7b7002d0fcda66558
Nadira Ali <nadiraali188a@gmail.com>
2022-05-06 18:26:49 +0100

    user model properties completed

--- models/User.js
+++ models/User.js
@@ -5,15 +5,23 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    trim: true 
+    trim: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
     validate: {
-        validator: () => Promise.resolve(false),
-        message: 'Email validation failed'
-      }
+      validator: () => Promise.resolve(false),
+      message: "Email validation failed",
+    },
+  },
+  thoughts: {
+    type: Schema.Types.ObjectId,
+    ref: "Thought",
+  },
+  friends: {
+    type: Schema.Types.ObjectId,
+        ref: 'User',
   }
 });
